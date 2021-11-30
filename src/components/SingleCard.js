988c3e38b8317fb058dd9c7a621f154b2fa7f1f7
Nikhil <nkmnikhil0009@gmail.com>
2021-11-30 18:45:46 +0530

    Update mobile view

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -14,7 +14,7 @@ export default function SingleCard({card, handleChoice, flipped, disabled}) {
                 <img className="front" src={card.src} alt="card front" />
                 <img 
                     className="back" 
-                    src="/img/cover.png" 
+                    src="/img/cover.jpg" 
                     alt="card back" 
                     onClick={handleClick} />
             </div>
